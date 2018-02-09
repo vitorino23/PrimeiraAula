d389f018bd13d045ce6779eb80e70cca0e5524b2
logonrm <logonrm@fiap.com.br>
2018-02-09 09:57:13 -0200

    Initial commit

---
+++ app/src/main/java/br/com/vitorcesario/primeiraaula/MainActivity.kt
@@ -0,0 +1,19 @@
+package br.com.vitorcesario.primeiraaula
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        botao.setOnClickListener {
+            val geradorNumero = Random()
+            label.text = geradorNumero.nextInt(50).toString()
+        }
+    }
+}
